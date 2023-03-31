94280db93693c52c205f937563bd78c3fd82f3bd
VrikkaDev <vrikkadev@gmail.com>
2023-03-31 19:01:14 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,28 @@
+# Simple 3D Rendering Engine
+
+This is a simple 3D rendering engine built using C++ and OpenGL.
+## Features
+- Window and input management using GLFW
+- Modern OpenGL rendering pipeline
+- Basic 3D object rendering
+- Simple camera and transformation controls
+
+## Dependencies
+- C++11 or higher
+- OpenGL 3
+- GLFW3
+- GLM
+- Glad
+
+## Building
+To build the project run:
+
+    git clone https://github.com/VrikkaDev/OpenGLRenderer.git
+    cd OpenGLRenderer
+    cmake -G "Unix Makefiles" .
+    make
+## Contributing
+Contributions are welcome! Feel free to open an issue or pull request.
+
+## License
+This project is licensed under the MIT License
--- src/Graphics/Model.cpp
+++ src/Graphics/Model.cpp
@@ -7,19 +7,6 @@
 Model::Model(Mesh& mesh, Shader& shader) : m_Mesh(mesh), m_Shader(shader){
 }
 
-static void print(glm::mat4 mat4){
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            std::cout << mat4[i][j] << " ";
-        }
-
-        // Newline for new row
-        std::cout << std::endl;
-    }
-}
-
 void Model::Render(Transform* transform, Camera* camera) {
     // Bind the shader program for rendering
     m_Shader.Bind();
@@ -38,18 +25,10 @@ void Model::Render(Transform* transform, Camera* camera) {
     auto proj = camera->GetProjectionMatrix();
     auto trans = matrix;
 
-    print(proj);
-
-    // Set model-view-projection matrix as uniform in shader
-    //m_Shader.LoadUniform("MVP", mvp);
-
+    // Load the matrices to the shader
     m_Shader.LoadUniform("viewMatrix", view);
     m_Shader.LoadUniform("projectionMatrix", proj);
     m_Shader.LoadUniform("transformationMatrix", trans);
-
-    // Set the OpenGL model-view matrix
-    //glMatrixMode(GL_MODELVIEW);
-    //glLoadMatrixf(glm::value_ptr(mvp));
 
     // Bind mesh VAO and draw
     glBindVertexArray(m_Mesh.getVAO());
